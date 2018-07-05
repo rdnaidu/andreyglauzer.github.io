134b42ef1a55ad056bfdede1a52c454f13a637d4
andrey <andrey@bind.com.br>
2018-07-05 10:57:57 -0300

    first commit

--- _scripts/cibuild.sh
+++ _scripts/cibuild.sh
@@ -10,8 +10,6 @@ set -e
 rm -rf _site
 mkdir _site
 
-$DATA = 'date +%d/%m/%Y'
-
 
 git clone https://${GH_TOKEN}@github.com/andreyglauzer/andreyglauzer.github.io.git --branch master _site
 
@@ -21,5 +19,5 @@ cd _site
 git config --global user.email "nglauzer@gmail.com"
 git config --global user.name "Andrey Glauzer"
 git add -A
-git commit -m "Commit feito pelo Travis #$TRAVIS_BUILD_NUMBER em $DATA"
+git commit -m "Commit feito pelo Travis #$TRAVIS_BUILD_NUMBER"
 git push origin master --force
